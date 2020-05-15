87076f000ceadc7fd0e4d195db2dbd21f740cbe0
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-15 19:19:52 -0300

    correção:Leitura do volume

--- src/prova1/prova.java
+++ src/prova1/prova.java
@@ -11,15 +11,22 @@ public class prova {
 		int ap;
 		double tm;
 		
+		// CABECALHO
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		System.out.println();
-		System.out.printf("Quantas vezes o cliente atrasou o pagamento? ");
+		
+		// LEITURA VOLUME
+		System.out.printf("Quantas comprar o cliente fez no último ano? ");
 		ap = sc.nextInt();
 		System.out.printf("Qual o ticket médio? ");
 		tm = sc.nextDouble();
 		
+		// LEITURA DOS OUTROS DADOS
+		
+		System.out.println();
 		
 		
 		sc.close();
